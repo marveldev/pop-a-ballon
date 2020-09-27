85e2ec8aeb675cfd32dec1d6579c0bcbd69c404d
Marvel Dev <marvellousalika48@gmail.com>
2020-09-27 14:28:18 +0100

    -add ballon class

--- class.js
+++ class.js
@@ -32,7 +32,7 @@ class Button {
 }
 
 class PoppedBallon {
-  constructor() {
+  confirmPoppedBallon() {
     if (poppedBallon === 12) {
       for (let index = 0; index < popMessages.length; index++) {
         const popMessage = popMessages[index];
@@ -45,26 +45,62 @@ class PoppedBallon {
 }
 
 class Ballon {
+  nextStage(selector) {
+    nextStageButton.addEventListener('click', () => {
+      let poppedBallon = 0;
+      for (let index = 0; index < ballons.length; index++) {
+        const ballon = ballons[index];
+        ballon.style.display = 'block';
+       
+        ballon.addEventListener('click', () => {
+          const popSound = new Audio("./Balloon_Pop_Sound.mp3");
+          popSound.play();
+          ballon.style.display = 'none';
+          ballon.nextElementSibling.style.display = 'block';
+          
+          poppedBallon++
+          if (poppedBallon === 12) {
+            for (let index = 0; index < popMessages.length; index++) {
+              const popMessage = popMessages[index];
+              popMessage.style.display = 'none';
+            }
+            document.querySelector('.next-stage-message').style.display = 'block';
+            nextStageButton.style.display = 'block';
+          }
+        })
+      }
+      document.querySelector('.next-stage-message').style.display = 'none';
+      nextStageButton.style.display = 'none';
+    })
+  }
+
   click() {
     for (let index = 0; index < ballons.length; index++) {
       const ballon = ballons[index];
       ballon.addEventListener('click', () => {
+        
         const popSound = new Audio("./Balloon_Pop_Sound.mp3");
         popSound.play();
         ballon.style.display = 'none';
         ballon.nextElementSibling.style.display = 'block';
+        
         poppedBallon++
-        const thing = new PoppedBallon();
-        thing.checkPoppedBallon(poppedBallon);
 
-        const something = new Button();
-        something.confirmNextStage();
+        const stuff = new PoppedBallon();
+        stuff.confirmPoppedBallon(poppedBallon);
+
+        // const thing = new PoppedBallon();
+        // thing.checkPoppedBallon(poppedBallon);
+
+        // const something = new Button();
+        // something.confirmNextStage();
       })
     }
   }
 }
 
-const stuff = new Ballon();
-stuff.click();
+const ballon = new Ballon();
+ballon.click();
+ballon.nextStage();
 
 
