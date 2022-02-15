9881eae4e31af75471ea1676e456b78d1bcd38ff
marveldev <marvellousalika48@gmail.com>
2022-02-15 16:15:03 +0100

    modify dashboard design

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -4,28 +4,32 @@ const Dashboard = ({ setIsSettings }) => {
   return (
     <div className={"overlay position-fixed w-100 h-100"}>
       <motion.div
-        className={"statistics card position-absolute rounded"}
+        className={"dashboard modal-dialog rounded"}
         initial={{ scale: 0.7, y:-200 }}
         animate={{ scale: [0.7, 1.5, 1], y: 200}}
         transition={{ duration: 0.8 }}
       >
-        <div className="card-header d-flex align-items-center">
-          <h3>Statistics</h3>
-          <button
-            className="btn-close"
-            aria-label="close"
-            onClick={() => setIsSettings(false)}
-          />
-        </div>
+        <div className="modal-content pb-2">
+          <div className="modal-header">
+            <h3>Dashboard</h3>
+            <button
+              className={"btn p-0"}
+              aria-label="close"
+              onClick={() => setIsSettings(false)}
+            >
+              <i className="material-icons fs-2">close</i>
+            </button>
+          </div>
 
-        <div className={"text-center p-3"}>
-          <div className={"my-3 fs-5"}>
-            <span className={"fw-bold"}>Highest score:</span> 0
+          <div className={"modal-body text-center"}>
+            <div className={"fs-5"}>
+              <span className={"fw-bold"}>Your score:</span> 0
+            </div>
+            <div className={"my-3 fs-5"}>
+              <span className={"fw-bold"}>Highest score:</span> 0
+            </div>
+            <button className={"btn btn-primary w-50"}>Reset</button>
           </div>
-          <div className={"my-4 fs-5"}>
-            <span className={"fw-bold"}>Your score:</span> 0
-          </div>
-          <button className={"btn btn-primary w-50"}>Reset</button>
         </div>
       </motion.div>
     </div>
