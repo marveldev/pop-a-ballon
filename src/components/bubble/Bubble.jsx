968169da9379c4715b92b7edf09bee409a66f678
marveldev <marvellousalika48@gmail.com>
2022-02-20 17:37:40 +0100

    create and design the bubble component

--- src/components/bubble/Bubble.jsx
+++ src/components/bubble/Bubble.jsx
@@ -1,45 +1,41 @@
 import './bubble.scss'
 
 const Bubble = () => {
-  const data = ['b-one jcc', 'b-two', 'b-three jcc', 'b-four', 'b-five', 'b-six', 'b-seven jcr', 'b-eight jcr', 'b-nine']
-  const width = window.innerWidth
-  console.log(width)
-
   return (
-    <div className={"bubble-wrap"}>
+    <div className={"bubble-wrap w-100 h-100"}>
       <div className={"header fw-bold d-flex justify-content-between"}>
         <p>Level <span>One</span></p>
         <p>Count Down: <span className={"fw-normal"}>00:00:00</span></p>
         <p>Balls Popped: <span className={"fw-normal"}>0</span></p>
       </div>
 
-      <div className="content">
+      <div className="bubbles d-grid position-absolute w-100">
         <div className="justify-content-center">
           <div className={"bubble"}/>
         </div>
         <div className="m-auto">
-          <div className={"bubble scalex3"}/>
-          <div className={"bubble scalex1"}/>
+          <div className={"bubble scaleX3"}/>
+          <div className={"bubble scaleX1"}/>
         </div>
         <div className="justify-content-center">
-          <div className={"bubble scalex2"}/>
+          <div className={"bubble scaleX2"}/>
         </div>
-        <div className="">
-          <div className={"bubble scalex1"}/>
+        <div>
+          <div className={"bubble scaleX1"}/>
         </div>
-        <div className="">
+        <div>
           <div className={"bubble"}/>
         </div>
-        <div className="">
-          <div className={"bubble scalex2"}/>
-          <div className={"bubble scalex1"}/>
+        <div>
+          <div className={"bubble scaleX2"}/>
+          <div className={"bubble scaleX1"}/>
         </div>
         <div className="m-auto">
-          <div className={"bubble scalex3"}/>
-          <div className={"bubble scalex2"}/>
+          <div className={"bubble scaleX3"}/>
+          <div className={"bubble scaleX2"}/>
         </div>
         <div className="m-auto">
-          <div className={"bubble scalex1"}/>
+          <div className={"bubble scaleX1"}/>
         </div>
         <div className="m-auto">
           <div className={"bubble"}/>
