e8c770dc5797ba9b4be655f485775d1cd186cbe3
marveldev <marvellousalika48@gmail.com>
2022-02-20 21:28:26 +0100

    create and design the floating bubbles

--- src/components/bubble/Bubble.jsx
+++ src/components/bubble/Bubble.jsx
@@ -1,49 +1,23 @@
 import './bubble.scss'
 
 const Bubble = () => {
+  const bubArray = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
   const popBubble = () => {
     
   }
 
   return (
     <div className={"bubble-wrap w-100 h-100"}>
-      <div className={"header fw-bold d-flex justify-content-between"}>
+      <div className={"header text-white fw-bold d-flex justify-content-between"}>
         <p>Level <span>One</span></p>
         <p>Count Down: <span className={"fw-normal"}>00:00:00</span></p>
         <p>Balls Popped: <span className={"fw-normal"}>0</span></p>
       </div>
 
-      <div className="bubbles d-grid position-absolute w-100">
-        <div className="justify-content-center">
-          <div className={"bubble"}/>
-        </div>
-        <div className="m-auto">
-          <div className={"bubble scaleX3 lg"}/>
-          <div className={"bubble scaleX1"}/>
-        </div>
-        <div className="justify-content-center">
-          <div className={"bubble scaleX2"}/>
-        </div>
-        <div>
-          <div className={"bubble scaleX1"}/>
-        </div>
-        <div>
-          <div className={"bubble"}/>
-        </div>
-        <div>
-          <div className={"bubble scaleX2"}/>
-          <div className={"bubble scaleX1"}/>
-        </div>
-        <div className="m-auto">
-          <div className={"bubble scaleX3 lg"}/>
-          <div className={"bubble scaleX2"}/>
-        </div>
-        <div className="m-auto">
-          <div className={"bubble scaleX1"}/>
-        </div>
-        <div className="m-auto">
-          <div className={"bubble"}/>
-        </div>
+      <div className="bubbles">
+        {bubArray.map((value, index) => (
+          <span key={index} className={`bub ${value}`} />
+        ))}
       </div>
     </div>
   )
