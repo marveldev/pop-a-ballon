d9acb1da22a7c087b6c0cf3c17f339a208b3cc17
marveldev <marvellousalika48@gmail.com>
2022-02-27 20:08:41 +0100

    create countdown timer

--- src/components/bubble/Bubble.jsx
+++ src/components/bubble/Bubble.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from 'react'
 import pop from './pop.mp3'
 import './bubble.scss'
 
 const Bubble = () => {
   const bubArray = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten']
   const audioElement = new Audio(pop)
+  const [counter, setCounter] = useState(40)
+
+  useEffect(() => {
+    counter > 0 && setTimeout(() => setCounter(counter - 1), 1000)
+    counter === 0 && alert('game over')
+  }, [counter])
 
   const popBubble = value => {
     const bubble = document.querySelector(`.${value}`)
@@ -25,7 +32,9 @@ const Bubble = () => {
     <div className={"bubble-wrap w-100 h-100"}>
       <div className={"header text-white fw-bold d-flex justify-content-lg-evenly"}>
         <p>Level: <span className={"fw-normal"}>One</span></p>
-        <p>Count Down: <span className={"fw-normal"}>00:00:00</span></p>
+        <p>Count Down:
+          <span className={"fw-normal"}> 00:00:{counter < 10 ? `0${counter}` : counter}</span>
+        </p>
         <p>Bubbles: <span className={"fw-normal"}>0/20</span></p>
       </div>
 
