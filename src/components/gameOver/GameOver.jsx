dfda9b6aca7e57f46e6c2df489af7b67468b7951
marveldev <marvellousalika48@gmail.com>
2022-03-04 20:36:13 +0100

    add game over content for when user wins a level

--- src/components/gameOver/GameOver.jsx
+++ src/components/gameOver/GameOver.jsx
@@ -37,16 +37,32 @@ const GameOver = () => {
       />
 
       {bubblesCount.get().bubblesPopped >= bubblesCount.get().totalToBePopped ? (
-        <div>
-          congratulations you passed.
-          <button className={"btn btn-danger"}>begin next level</button>
-          <button className={"btn btn-success"}>back to home</button>
+        <div className="card text-center bg-transparent">
+          <div className="card-header text-danger fw-bold p-0">
+            <p>😃</p>
+            <p className={"lh-1"}>You Win</p>
+          </div>
+
+          <div className="card-body">
+            <p>Your Score: 366</p>
+            <p className="card-title">Highest Score: 366</p>
+
+            <div className={"d-flex justify-content-center gap-4 mt-4"}>
+              <button className="btn rounded-circle text-white">
+                <i className="material-icons">fast_forward</i>
+              </button>
+
+              <button className="btn rounded-circle text-white">
+                <i className="material-icons">home</i>
+              </button>
+            </div>
+          </div>
         </div>
       ) : (
         <div className="card text-center bg-transparent">
           <div className="card-header text-danger fw-bold p-0">
             <p>😞</p>
-            <p className={"lh-1"}>Game Over</p>
+            <p className={"lh-1"}>You Lose</p>
           </div>
 
           <div className="card-body">
