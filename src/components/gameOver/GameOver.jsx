f6bcaeea39462356967b3eb021a2b30f59438a3e
marveldev <marvellousalika48@gmail.com>
2022-03-04 09:39:37 +0100

    create and design game over component

--- src/components/gameOver/GameOver.jsx
+++ src/components/gameOver/GameOver.jsx
@@ -1,11 +1,12 @@
 import { useState as reactHookState } from '@hookstate/core'
 import store from '../../store'
+import './gameOver.scss'
 
 const GameOver = () => {
   const { bubblesCount } = reactHookState(store)
 
   return (
-    <div>
+    <div className={"game-over m-auto"}>
       {bubblesCount.get().bubblesPopped >= bubblesCount.get().totalToBePopped ? (
         <div>
           congratulations you passed.
@@ -13,9 +14,26 @@ const GameOver = () => {
           <button className={"btn btn-success"}>back to home</button>
         </div>
       ) : (
-        <div>
-          Game over, sorry you failed.
-          <button className={"btn btn-success"}>back to home</button>
+        <div className="card text-center bg-transparent">
+          <div className="card-header text-danger fw-bold p-0">
+            <p>😞</p>
+            <p className={"lh-1"}>Game Over</p>
+          </div>
+
+          <div className="card-body">
+            <p>Your Score: 366</p>
+            <p className="card-title">Highest Score: 366</p>
+
+            <div className={"d-flex justify-content-center gap-4 mt-4"}>
+              <button className="btn rounded-circle text-white">
+                <i className="material-icons">replay</i>
+              </button>
+
+              <button className="btn rounded-circle text-white">
+                <i className="material-icons">home</i>
+              </button>
+            </div>
+          </div>
         </div>
       )}
     </div>
